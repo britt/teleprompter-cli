a40d4f2311cbe946618147584cf1f9161657babc
Britt Crawford <britt.v.crawford@gmail.com>
2025-11-08 09:13:17 -0800

    remove aborted v2 version

--- teleprompter-cli-v2/source/cli.tsx
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import program from './commands/index.js';
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-	program.parse(process.argv);
-}
-
-export default program;
